6bedb8fc2334c9df6b2c6acf87da549506443a77
squaresgit <tatoomark@outlook.com>
2024-04-15 21:21:14 -0700

    travis push #3 to AWS

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React. Its the best.
+            Learn React. Its the best. No really.
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
